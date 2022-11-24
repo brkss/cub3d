4c3932969aa33973fdcbac2d2b6ac6d7a337e496
Brahim Berkasse <berkassebrahim@gmail.com>
2022-11-24 16:06:50 +0100

    🍞 FIX gnl

--- src/check_map.c
+++ src/check_map.c
@@ -134,6 +134,8 @@ int check_player(char **map)
   while(map[i])
   {
     j = 0;
+    while(map[i][j] && map[i][j] != ' ')
+        j++;
     while(map[i][j])
     {
       if(map[i][j] == 'N' || map[i][j] == 'W'
@@ -143,7 +145,9 @@ int check_player(char **map)
           return (0);
         player_exist = 1;
       }
-      else if(map[i][j] != '1' && map[i][j] != '0')
+      else if (map[i][j] == '0' &&  map[i][j + 1] && map[i][j] == ' ')
+            return (0);
+      else if(map[i][j] != '1' && map[i][j] != '0' && j > 0 && map[i][j - 1] != '1')
       {
         return (0);
       }
